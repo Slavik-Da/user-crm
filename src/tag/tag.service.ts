e33cef81f99495dd763e2b10dff6e63a7db85cff
Stanislav Davydenko <buhtobo4kuh@gmail.com>
2023-05-03 15:05:05 +0300

    draft for tag service augmented

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@nestjs/common";
 @Injectable()
 export class TagService {
-  async createTag(name: string, color: string) {
-    return {name, color}
+  async createUserTag(userIdFromJWT: string, userIdFromParams: string,name: string, color: string) {
+    return {name, color, userIdFromJWT}
   }
 
 }
