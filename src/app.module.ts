5b11d2c38f938dc6a822402a4384d1a216a81444
Stanislav Davydenko <buhtobo4kuh@gmail.com>
2023-05-03 15:30:02 +0300

    tag module connected to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UsersModule } from "./users/users.module";
 import { AuthModule } from "./auth/auth.module";
 import { DatabaseModule } from "./database/database.module";
 import { OrderModule } from "./order/order.module";
+import { TagModule } from "./tag/tag.module";
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { OrderModule } from "./order/order.module";
     UsersModule,
     AuthModule,
     OrderModule,
+    TagModule
   ],
   controllers: [],
   providers: [],
