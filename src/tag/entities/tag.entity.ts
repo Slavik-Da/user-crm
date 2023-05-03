37c65d9d911265fc58ea4d5ab985b746e3d1f620
Stanislav Davydenko <buhtobo4kuh@gmail.com>
2023-05-03 21:08:44 +0300

    base for tag entity created

---
+++ src/tag/entities/tag.entity.ts
@@ -0,0 +1,17 @@
+import {
+  Entity,
+  Column, Index
+} from "typeorm";
+import { BaseEntity } from "../../common/entities/baseEntity";
+
+
+@Entity()
+@Index(["name", "color"], { unique: true })
+export class Tag extends BaseEntity {
+  @Column()
+  name: string;
+
+  @Column()
+  color: string;
+
+}
