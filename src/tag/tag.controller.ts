bc1012f0c3affceda2c150d03f658d4447f8a8b8
Stanislav Davydenko <buhtobo4kuh@gmail.com>
2023-05-03 15:05:26 +0300

    draft for tag controller created

---
+++ src/tag/tag.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, HttpCode, HttpStatus, Param, Post, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { UserDecorator } from "../auth/decorators/user.decorator";
+import { JwtPayload } from "../types";
+import { TagService } from "./tag.service";
+
+@Controller()
+export class TagController {
+  constructor(
+    private readonly tagService: TagService
+  ) {}
+
+  @Post("users/:userId/tags")
+  @UseGuards(AuthGuard("jwt"))
+  @HttpCode(HttpStatus.CREATED)
+  async createUserTag(
+    @Body() newTag,
+    @UserDecorator() user: JwtPayload,
+    @Param("userId") userId: string
+  ) {
+    return await this.tagService.createUserTag(user.sub, userId, newTag.name, newTag.color);
+  }
+
+}
