2c313cef4daf577817128178a0e2400c8790fbf0
Stanislav Davydenko <buhtobo4kuh@gmail.com>
2023-05-08 11:46:26 +0300

    TagDAO and UserDAO connected to tag module

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -3,11 +3,14 @@ import { TagController } from "./tag.controller";
 import { TagService } from "./tag.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Tag } from "./entities/tag.entity";
+import { TagDAO } from "./dao/tagDAO";
+import { UsersDAO } from "../users/dao/usersDAO";
+import { User } from "../users/entities/users.entity";
 
 @Module({
   controllers: [TagController],
-  providers: [TagService],
-  imports: [TypeOrmModule.forFeature([Tag])],
+  providers: [TagService, TagDAO, UsersDAO],
+  imports: [TypeOrmModule.forFeature([Tag, User])],
   exports: [],
 })
 export class TagModule {}
